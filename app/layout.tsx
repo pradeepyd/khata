d07fea9ae60767d8bd7fa777ed10022598b69c28
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-08 23:12:06 +0545

    mnemonics and wallet done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import {Poppins} from 'next/font/google';
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '700'], // choose weights you need
+  display: 'swap',
+});
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -25,13 +31,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className= {`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className= {`${geistSans.variable} ${geistMono.variable} antialiased ${poppins.className} ${"bg-black"}`}
       >
-        <div className=" h-full w-full bg-slate-950"><div className="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#4f4f4f2e_1px,transparent_1px)] bg-[size:14px_24px] [mask-image:radial-gradient(ellipse_60%_50%_at_50%_0%,#000_70%,transparent_100%)]"></div>
-    
         {children}
-
-</div>
       </body>
     </html>
   );
