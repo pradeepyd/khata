0776a0c026c00a51d2f88bf44733fb511cdc3092
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-10 23:34:55 +0545

    mnemonic card blur made

--- components/MnemonicCard.tsx
+++ components/MnemonicCard.tsx
@@ -5,10 +5,11 @@ import { SolWallets } from "./SolWallet";
 import { EthWallet } from "./EthWallet";
 import { Eye, EyeOff, ChevronDown, ChevronUp } from "lucide-react";
 
-const MnemonicCard = ({selectedWallet}:any) => {
+const MnemonicCard = () => {
   const [mnemonic, setMnemonic] = useState("");
   const [showMnemonic, setShowMnemonic] = useState(false);
   const [isExpanded, setIsExpanded] = useState(false);
+  const [selectedWallet, setSelectedWallet] = useState("");
   const words = mnemonic.split(" ");
 
   async function generateMenonics() {
@@ -18,18 +19,52 @@ const MnemonicCard = ({selectedWallet}:any) => {
     setShowMnemonic(false);
   }
 
-  return (
-    <div className="text-white px-10">
-      {/* Create Seed Phrase button (disappears after generation) */}
-      {!mnemonic && (
-        <button
-          onClick={generateMenonics}
-          className="bg-gray-800 px-4 mt-4 mb-4 py-2 rounded border"
-        >
-          Create Seed Phrase
-        </button>
-      )}
+  const handleWalletChange = (e: any) => {
+    setSelectedWallet(e.target.value);
+  };
 
+  return (
+    <div className="text-[#F5F5F5] px-10">
+
+      <h2 className="text-gray-300 font-normal mb-4">
+        Select the blockchain you want to create an account
+      </h2>
+      <div className="flex flex-col md:flex-row justify-between items-start w-full">
+        <div className="flex flex-col  space-y-2 mb-6">
+          <label className="flex items-center space-x-3">
+            <input
+              type="checkbox"
+              name="wallet"
+              value="solana"
+              checked={selectedWallet === "solana"}
+              onChange={handleWalletChange}
+              className="form-radio h-5 w-5 text-blue-600"
+            />
+            <span className="text-white/85">Solana Wallet</span>
+          </label>
+
+          <label className="flex items-center space-x-3">
+            <input
+              type="checkbox"
+              name="wallet"
+              value="ethereum"
+              checked={selectedWallet === "ethereum"}
+              onChange={handleWalletChange}
+              className="form-radio h-5 w-5 text-blue-600"
+            />
+            <span className="text-white/85">Ethereum Wallet</span>
+          </label>
+        </div>
+
+      
+        {!mnemonic && (<button
+          onClick={generateMenonics}
+          disabled={!selectedWallet}
+          className="bg-gray-600 px-4 mt-4 mb-4 py-2 rounded border"
+        >
+          Generate Wallet
+        </button>)}
+      </div>
       {mnemonic && (
         <div className="group flex flex-col cursor-pointer rounded-lg border border-gray-600 px-8 justify-between">
           <div className="flex items-center justify-between">
@@ -40,20 +75,12 @@ const MnemonicCard = ({selectedWallet}:any) => {
             <div className="flex space-x-4">
               {/* Eye icon */}
               <button onClick={() => setShowMnemonic(!showMnemonic)}>
-                {showMnemonic ? (
-                  <Eye size={20} />
-                ) : (
-                  <EyeOff size={20} />
-                )}
+                {showMnemonic ? <Eye size={20} /> : <EyeOff size={20} />}
               </button>
 
               {/* Chevron icon */}
               <button onClick={() => setIsExpanded(!isExpanded)}>
-                {isExpanded ? (
-                  <ChevronUp size={20} />
-                ) : (
-                  <ChevronDown size={20} />
-                )}
+                {isExpanded ? <ChevronUp size={20} /> : <ChevronDown size={20} />}
               </button>
             </div>
           </div>
@@ -63,9 +90,8 @@ const MnemonicCard = ({selectedWallet}:any) => {
               {words.map((word, index) => (
                 <p
                   key={index}
-                  className={`md:text-lg flex items-center rounded-lg px-3 py-3 space-x-2 border border-gray-600 bg-gray-900 shadow-inner text-sm transition-all duration-500 ${
-                    showMnemonic ? "blur-none" : "blur-sm"
-                  }`}
+                  className={`md:text-lg flex items-center rounded-lg px-3 py-3 space-x-2 border border-gray-600 bg-[#0B1215] shadow-inner text-sm transition-all duration-500 ${showMnemonic ? "blur-none" : "blur-sm"
+                    }`}
                 >
                   {index + 1}.&nbsp;{word}
                 </p>
