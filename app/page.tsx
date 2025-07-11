fc5c6ea09a3df8efd0c79eb0aa1072020072f44c
Pradeep yadav <pradeep20020102@gmail.com>
2025-07-11 13:15:27 +0545

    footer added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import MnemonicCard from "@/components/MnemonicCard";
 
 export default function Home() {
@@ -7,6 +8,9 @@ export default function Home() {
       <h1 className="text-3xl font-semibold space-x-2 ml-10 gap-2 py-2 space-y-2">Welcome to khata </h1>
       <p className="text-md ml-10 text-gray-400 mb-4">create your web3 wallet seamlessly</p>
       <MnemonicCard/>
+      <div className="relative mt-30 items-center text-center">
+      <Footer/>
       </div>
+    </div>
   );
 }
